186445a40df52a3f0f6ea7be7e55774d38269ce7
KasiVisalakshiBorra <KasiVisalakshiBorra@9.85.89.17>
2020-03-14 13:10:32 +0530

    ne

--- eclise project/pra/src/visala/hi.java
+++ eclise project/pra/src/visala/hi.java
@@ -5,6 +5,8 @@ public class hi {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("creted 2nd class");
+System.out.println("creted new workspace");
+System.out.println("creted checki-1scdc ");
 	}
 
 }
